330998948af1a17fd152fe928345e2e58f9095c3
eran <eran.shemesh@fyber.com>
2018-09-23 00:47:45 +0300

    fixing main

--- __main__.py
+++ __main__.py
@@ -2,9 +2,10 @@ from BaseHTTPServer import HTTPServer
 
 from app.meir.http_requests_hendler import HttpRequestsHandler
 
-SERVER_PORT = 80
+SERVER_PORT = 8080
 
 def start_server(server_class=HTTPServer, handler_class=HttpRequestsHandler, port=SERVER_PORT):
+    print "starting server"
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
--- app/meir/http_requests_hendler.py
+++ app/meir/http_requests_hendler.py
@@ -4,10 +4,20 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from cStringIO import StringIO
 import logging
 
+from app.meir.html_page_fetching.lessons_page_iterator import LessonsPageIterator
+from app.meir.html_page_fetching.lessons_page_iterator_factory import LessonsPageIteratorFactory
+from app.meir.html_page_fetching.web_driver_proxy import WebDriverProxy
+from app.meir.html_page_fetching.web_driver_repository import WebDriverRepository
 from app.meir.request_handler import RequestHandler
+from app.meir.scraping.html_lessons_page_scraper import LessonsPageScraper
 
 
 class HttpRequestsHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    lessons_page_scraper = LessonsPageScraper()
+    web_drivers_repository = WebDriverRepository()
+    web_driver_proxy = WebDriverProxy(web_drivers_repository)
+    lessons_page_iterator_factory = LessonsPageIteratorFactory(web_driver_proxy)
 
     def _set_headers(self):
         self.send_response(200)
@@ -15,15 +25,13 @@ class HttpRequestsHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        print "do_get"
         self._set_headers()
         logging.info("started to handle request")
-        # request_handler = RequestHandler("http://meirtv.co.il/site/set.asp?id=22939")
-        # lessons_rss = request_handler.get_lessons_rss()
+        request_handler = RequestHandler("http://meirtv.co.il/site/set.asp?id=22939",
+                                         self.lessons_page_scraper,
+                                         self.lessons_page_iterator_factory)
+        lessons_rss = request_handler.get_lessons_rss()
 
         logging.info("finnished to handle request")
-        self.wfile.write(self.get_expected_rss_file())
-
-    def get_expected_rss_file(self):
-        with open("../../tests/expected_rss.xml", "r") as myfile:
-            data = myfile.read()
-        return data
+        self.wfile.write(lessons_rss)
